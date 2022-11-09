d2706306d41fd2c0255fcd270d598767773d9894
schmellerl <65337849+schmellerl@users.noreply.github.com>
2022-11-09 11:34:08 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,48 @@
+#
+# DEFINE PARAMETERS
+#
+r  = 2.0
+h  = 5.0
+H1 = 10.0
+H2 = 10.0
+
+# CONFLICTING THOSE
+
+# Physics
+sigma1         = 10.0    # solid/liquid
+sigma2         = 20.0    # solid/air
+sigma3         = 30.0    # liquid/air
+mu             = 1.00 # Stokes Dissipation
+kappa          = 1000.0
+G0 = 0.01    # air
+G1 = 0.01    # liquid
+G2 = 200.0   # solid
+
+# Output
+n_start        = 20             # number of warm-up steps
+n_out          = 10             # frequency of output
+filepath       = 'output_Leonie/'
+
+# Solver
+NEWTON_tol     = 1e-7
+tol            = 1e-9
+
+# Discretization
+FEu_DEG        = 2
+FEp_DEG        = 1
+FEs_DEG        = 2
+FE_quad        = 5
+
+tau            = 0.0005  
+n_steps        = 250
+max_level      = 9
+ceps           = 0.85
+
+# Switches
+INCOMPRESSIBLE = False
+MOBILITY       = False
+RADIAL         = False
+
 from dolfin import *
 from mshr import *
 from ufl import cofac
