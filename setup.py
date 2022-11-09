ee9f2b4ea687a588762b9f92826e6968bc4fe98b
schmellerl <65337849+schmellerl@users.noreply.github.com>
2022-11-09 10:04:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -58,3 +58,80 @@ n  = FacetNormal(mesh)
 dS = Measure("dS", domain=mesh, subdomain_data=interface_marker)
 dx = Measure("dx", domain=mesh, subdomain_data=subdomain_marker)
 R  = SpatialCoordinate(mesh)
+
+parameters["form_compiler"]["optimize"] = True
+parameters["form_compiler"]["cpp_optimize"] = True
+parameters["form_compiler"]["quadrature_degree"] = FE_quad
+
+def get_space(mesh):
+  if FEu_DEG>0: # Standard element
+    Pv = VectorElement("P", mesh.ufl_cell(), FEu_DEG)
+  else: # Mini element
+    Pk = FiniteElement("Lagrange", mesh.ufl_cell(), -FEu_DEG)
+    B  = FiniteElement("Bubble",   mesh.ufl_cell(), 3)
+    Pv = VectorElement(NodalEnrichedElement(Pk, B))  
+
+  if FEp_DEG>0:
+    R  = FiniteElement("P", mesh.ufl_cell(), FEp_DEG)
+  else:
+    R  = FiniteElement("DG", mesh.ufl_cell(), -FEp_DEG)
+
+  if INCOMPRESSIBLE:
+      V   = FunctionSpace(mesh, MixedElement([Pv, R])) 
+  else:
+      V   = FunctionSpace(mesh,Pv)
+  return V
+
+def get_scalar(mesh):
+  Ps = FiniteElement("P", mesh.ufl_cell(), FEs_DEG)
+  S = FunctionSpace(mesh, Ps)
+  return S
+
+def get_vector(mesh): 
+  if FEu_DEG>0: # Standard element
+      Pv = VectorElement("P", mesh.ufl_cell(), FEu_DEG)
+  else: # Mini element
+      Pk = FiniteElement("Lagrange", mesh.ufl_cell(), -FEu_DEG)
+      B  = FiniteElement("Bubble",   mesh.ufl_cell(), 3)
+      Pv = VectorElement(NodalEnrichedElement(Pk, B))     
+  V = FunctionSpace(mesh, Pv)
+  return V
+
+noslip   = Constant((0, 0)) 
+noslip1d = Constant(0)
+
+def boundary_bot1(x, on_boundary):
+    return on_boundary and near(x[0], 0, tol)
+def boundary_bot2(x, on_boundary):
+    return on_boundary and near(x[1], 0, tol)
+def boundary_bot3(x, on_boundary):
+    return on_boundary and near(x[0], H1, tol)
+def boundary_bot4(x, on_boundary):
+    return on_boundary and near(x[1], H2, tol)
+
+def get_BC(W):   
+  if INCOMPRESSIBLE:
+    bc1 = DirichletBC(W.sub(0).sub(0) , noslip1d , boundary_bot1)
+    bc2 = DirichletBC(W.sub(0)        , noslip   , boundary_bot2)
+    bc3 = DirichletBC(W.sub(0).sub(0) , noslip1d , boundary_bot3)
+    bc4 = DirichletBC(W.sub(0)        , noslip   , boundary_bot4)
+  else:
+    bc1 = DirichletBC(W.sub(0), noslip1d, boundary_bot1)
+    bc2 = DirichletBC(W       , noslip  , boundary_bot2)
+    bc3 = DirichletBC(W.sub(0), noslip1d, boundary_bot3)
+    bc4 = DirichletBC(W       , noslip  , boundary_bot4)      
+  bc = [bc1,bc2,bc3,bc4]
+  return bc
+
+def scalar_grad_axi(u):
+    if RADIAL:
+        return as_vector([u.dx(0), 0, u.dx(1)])
+    else:
+        return grad(u)
+  
+def vector_grad_axi(v):
+    if RADIAL:
+        return as_tensor([[v[0].dx(0), 0, v[0].dx(1)], [0, v[0]/R[0], 0],
+                                    [v[1].dx(0), 0, v[1].dx(1)]])
+    else:
+        return grad(v)
